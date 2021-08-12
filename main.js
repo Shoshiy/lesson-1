8bb01255f333464afd1c5fa411474a7283f7ebae
Константин Тумаков <84858921+Shoshiy@users.noreply.github.com>
2021-08-12 18:44:00 +0300

    add files
    добавил все файлы
    своего немного, все очень интересно, но из-за проклятой работы практически нет времени на задания)

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
-'use strict';
-const products = [
-    {id: 1, title: 'Notebook', price: 1000},
-    {id: 2, title: 'Mouse', price: 100},
-    {id: 3, title: 'Keyboard', price: 250},
-    {id: 4, title: 'Gamepad', price: 150},
-];
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-const renderProduct = card => 
-     `<div class="product-card">
-                <h3>${card.title}</h3>
-                <p>${card.price}</p>
-                <button class="by-btn">Добавить</button>
-              </div>`;
+const app = new Vue({
+    el: '#app',
+    methods: {
+        getJson(url){
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                    this.$refs.error.setError(error);
+                })
+        },
+    },
+    mounted() {
+        console.log(this);
+    }
+});
 
-const renderProducts = list => {
-    document.querySelector('.products').insertAdjacentHTML("beforeend", list.map(card => renderProduct(card)).join(" "));
-    console.log(list);
-}
-
-renderProducts(products);
